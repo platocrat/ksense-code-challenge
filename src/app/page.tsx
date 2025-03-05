6453fddded4830d4f678afb411424401a26b716f
platocrat <jack.abe.winfield@gmail.com>
2025-03-05 11:58:24 -0600

    fix: add CSS styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,50 @@
+import styles from './page.module.css'
+
 export default function Home() {
   return (
-    <main className="prose mx-auto py-10">
-      <h1>Ksense Code Challenge - Webhook</h1>
+    <div className={ styles.page }>
+      <main className={ styles.main }>
+        <h1 className={ styles.title }>Ksense Code Challenge - Webhook</h1>
 
-      <p>
-        This project is a simple webhook built with <strong>Next.js</strong> and <strong>TypeScript</strong> to receive the Ksense Code Challenge payload.
-      </p>
+        <p>
+          This project is a simple webhook built with <strong>Next.js</strong> and <strong>TypeScript</strong> to receive the Ksense Code Challenge payload.
+        </p>
 
-      <h2>How it works</h2>
+        <h2>How it works</h2>
+        <ul>
+          <li>Accepts a POST request at <code>/api/webhook</code>.</li>
+          <li>Stores the entire payload in Next.js KV.</li>
+          <li>Logs and returns the payload in the response.</li>
+        </ul>
 
-      <ul>
-        <li>Accepts a POST request at <code>/api/webhook</code>.</li>
-        <li>Stores the entire payload in Next.js KV.</li>
-        <li>Logs and returns the payload in the response.</li>
-      </ul>
+        <h2>Deploy</h2>
+        <ol>
+          <li>Clone the repo.</li>
+          <li>Install dependencies:</li>
+        </ol>
+        <pre>
+          <code>npm i</code>
+        </pre>
+        <p>Run locally:</p>
+        <pre>
+          <code>npm run dev</code>
+        </pre>
+        <p>Deploy on Vercel.</p>
 
-      <h2>Deploy</h2>
+        <p>Use the Webhook URL:</p>
+        <pre>
+          <code>https://&lt;your-vercel-project&gt;.vercel.app/api/webhook</code>
+        </pre>
 
-      <ol>
-        <li>Clone the repo.</li>
-        <li>Install dependencies:</li>
-      </ol>
-
-      <pre>
-        <code>npm i</code>
-      </pre>
-
-      <p>Run locally:</p>
-
-      <pre>
-        <code>npm run dev</code>
-      </pre>
-
-      <p>Deploy on Vercel.</p>
-
-      <p>Use the Webhook URL:</p>
-
-      <pre>
-        <code>https://&lt;your-vercel-project&gt;.vercel.app/api/webhook</code>
-      </pre>
-
-      <h2>Test the Webhook</h2>
-
-      <p>Local testing with the <code>curl</code> command:</p>
-
-      <pre>
-        <code>{ `curl -X POST http://localhost:3000/api/webhook \\
+        <h2>Test the Webhook</h2>
+        <p>Local testing with the <code>curl</code> command:</p>
+        <pre>
+          <code>{ `curl -X POST http://localhost:3000/api/webhook \\
   -H "Content-Type: application/json" \\
   -d '{ "secretCode": "test1234" }'`}
-        </code>
-      </pre>
-    </main>
+          </code>
+        </pre>
+      </main>
+    </div>
   )
 }
